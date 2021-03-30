2a08a9f9501233fdd18f117611c6290247cbea96
Tuyen Truong <mtuyen2907@gmail.com>
2021-03-30 16:48:36 +0700

    Baitap useState1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.scss';
-
+import ColorBox from './component/ColorBox';
 function App() {
   return (
     <div className="App">
       <h1>WELLCOME</h1>
+
+      <ColorBox />
     </div>
 
   );
 }
+
 
 export default App;
---
+++ src/component/ColorBox/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ColorBox.scss';
+ColorBox.propTypes = {
+
+};
+function getRandomColor() {
+    const COLOR_LIST = [ '#cddc39', 'glod', '#03a9f4', '#607d8b', '#ffeb3bc7'];
+    const randomIndex = Math.trunc(Math.random()*5); //trunc hàm lấy số nguyên
+    return COLOR_LIST[randomIndex];
+}
+function ColorBox() {
+    const [color, setColor] = useState( () => {
+    const initColor = localStorage.getItem('box-color') || 'pink';
+        return initColor;
+
+    });  //initColor chỉ render 1 lần đầu tiên
+
+    function handleBoxClick(){
+        //get random color --> color
+        const newColor = getRandomColor(); //random ngẫu nhiên màu
+        setColor(newColor);
+        localStorage.setItem('box-color', newColor); //lưu giá trị màu lại
+    }
+    return (
+        <div className="color-box"
+            style={{ backgroundColor: color }}
+                onClick={handleBoxClick}
+        >
+            COLOR BOX
+            
+        </div>
+    );
+}
+
+export default ColorBox;
+
+/**
+ * 1.Khi click lên box đổi background ngẫu nhiên thành màu khác 
+ * 2. Giữ màu background của box sau khi reload trình duyệt
+ */
