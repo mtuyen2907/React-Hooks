a927878b18b6b2bb1127bf3edb77155e699fbde0
Tuyen Truong <mtuyen2907@gmail.com>
2021-04-01 11:31:58 +0700

    Bai tap useState2

---
+++ src/component/TodoList/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    todos: PropTypes.array, //phím tắt pta -> PropTypes.array,
+    onTodoClick: PropTypes.func,
+};
+//xét giá trị mặc định là bao nhieu
+TodoList.defaulProps = {
+    todos: [],   //cha k truyền todo xuống lấy giá trị mặc định là []
+    onTodoClick: null, //onTodoClick k đc truyền xuống lấy gía trị là null
+};
+function TodoList(props) {
+    const { todos, onTodoClick } = props;
+
+    function handleClick(todo) {
+        if (onTodoClick) {
+            onTodoClick(todo);
+        }
+    }
+    return (
+        <ul className="todo-list">
+            {todos.map(todo => (
+                <li key={todo.id}
+                    onClick={() => handleClick(todo)}
+                >
+                    {todo.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
